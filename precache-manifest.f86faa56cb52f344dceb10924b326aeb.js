cc86ee60616df3f30163562790507df0e7cca11f
Jon Bascos <jon@jonbascos.com>
2020-01-28 13:56:12 -0800

    Updates

---
+++ precache-manifest.f86faa56cb52f344dceb10924b326aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4daf26d86d95254f858abb574836d4",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "fa3e6bcc879eb0168e09",
+    "url": "/meme-generator/static/css/main.d4bade3c.chunk.css"
+  },
+  {
+    "revision": "a0174b71645ad1d7e90a",
+    "url": "/meme-generator/static/js/2.f12c11cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-generator/static/js/2.f12c11cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa3e6bcc879eb0168e09",
+    "url": "/meme-generator/static/js/main.21c580b2.chunk.js"
+  },
+  {
+    "revision": "4db86d4440860f2b5c7e",
+    "url": "/meme-generator/static/js/runtime-main.11fa8ce1.js"
+  }
+]);
